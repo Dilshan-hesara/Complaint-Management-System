891dff6ccd1a0362f9b4470b3821f6113178854e
Dilshan-hesara <hesarsdilshan@gmail.com>
2025-06-14 11:04:01 +0530

    add user dao

--- src/main/java/lk/dilshanhesara/dilshan/dao/UserDAO.java
+++ src/main/java/lk/dilshanhesara/dilshan/dao/UserDAO.java
@@ -1,4 +1,38 @@
 package lk.dilshanhesara.dilshan.dao;
 
+import lk.dilshanhesara.dilshan.db.DBCPDataSource;
+import lk.dilshanhesara.dilshan.model.User;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class UserDAO {
+
+    private User validateUser(String username, String password) {
+        String sql = "SELECT * FROM users WHERE username = ? AND password = ?";
+        User user = null;
+
+        try (Connection con = DBCPDataSource.getConnection();
+             PreparedStatement pst = con.prepareStatement(sql)) {
+
+            pst.setString(1, username);
+            pst.setString(2, password);
+
+            try (ResultSet rs = pst.executeQuery()) {
+                if (rs.next()) {
+                    user = new User();
+                    user.setId(rs.getInt("user_id"));
+                    user.setUsername(rs.getString("username"));
+                    user.setRole(rs.getString("role"));
+                }
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return user;
+    }
 }

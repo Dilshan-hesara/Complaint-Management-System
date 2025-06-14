55b4231213c41d52ca67865300d6205ffdc75faf
Dilshan-hesara <hesarsdilshan@gmail.com>
2025-06-14 12:57:06 +0530

    craete Complaint servlet

---
+++ src/main/java/lk/dilshanhesara/dilshan/controller/ComplaintServlet.java
@@ -0,0 +1,11 @@
+package lk.dilshanhesara.dilshan.controller;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+@WebServlet("/complaints")
+public class ComplaintServlet extends HttpServlet {
+
+
+
+}

2980b7de0344cea227af02e2f24a750590482f89
Dilshan-hesara <hesarsdilshan@gmail.com>
2025-06-15 09:15:04 +0530

    crate signup servlete

---
+++ src/main/java/lk/dilshanhesara/dilshan/controller/SignUpServlet.java
@@ -0,0 +1,8 @@
+package lk.dilshanhesara.dilshan.controller;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+@WebServlet("/signup")
+public class SignUpServlet extends HttpServlet {
+}
